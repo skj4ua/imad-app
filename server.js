badbb00fb74c00f977f514c706c81dfd6fad29de
sonu jha <sonukrj08@gmail.com>
2018-02-17 00:10:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,9 +12,19 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 app.get('/article-one',function(req,res){
     res.send("article one will be served here");
-})
+});
+    
+app.get('/article-two',function(req,res){
+    res.send("article two will be served here");
+});
+
+app.get('/article-three',function(req,res){
+    res.send("article three will be served here");
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });

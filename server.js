010540697022b0fafe27072c420bf992c1dccca8
sonu jha <sonukrj08@gmail.com>
2018-03-06 18:50:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/articles/:article', function (req, res) {
       if(err){
           res.status(500).send(err.toString());
       }  else{
-          if(result.rows.length===0){
+          if(result.rows.length==0){
               res.status(404).send('Article was found');
           }
           else{

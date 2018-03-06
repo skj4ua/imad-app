ab9158dc15111e008ef327cf8d31e6929e364735
sonu jha <sonukrj08@gmail.com>
2018-03-06 18:11:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var Pool = new require('pg').Pool;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });

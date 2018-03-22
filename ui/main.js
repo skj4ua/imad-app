1b0d9079f0750c91ef4742e6093a4c3a4aa88374
sonu jha <sonukrj08@gmail.com>
2018-03-22 16:16:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
 console.log('Loaded!');
+var reg = document.getElementById('reg_btn');
 var submit = document.getElementById('submit_btn');
 submit.on('click',function(req,res){
     var request = new XMLHttpRequest();
@@ -19,6 +20,26 @@ submit.on('click',function(req,res){
    var username = document.getElementById('username');
    var password = document.getElementById('password');
    request.open('POST','http://sonukrj08.imad.hasura-app.io/login',true);
+   request.setRequestHeader('content-type','application/json');
+   request.send(JSON.stringify({username:username,password:password}));
+});
+
+reg.on('click',function(req,res){
+    var request = new XMLHttpRequest();
+    
+    request.onreadystateexchange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status == 200)
+            alert("registration Successfully");
+            
+            else
+            alert("something went wrong on the server");
+        }
+    };
+    
+   var username = document.getElementById('username');
+   var password = document.getElementById('password');
+   request.open('POST','http://sonukrj08.imad.hasura-app.io/create-user',true);
    request.setRequestHeader('content-type','application/json');
    request.send(JSON.stringify({username:username,password:password}));
 });

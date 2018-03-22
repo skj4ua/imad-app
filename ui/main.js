510e86a9d414a3079898112b05dd6c537fc0b81f
sonu jha <sonukrj08@gmail.com>
2018-03-22 17:11:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,8 +20,8 @@ submit.onclick=function(){
         }
     };
     
-   var username = document.getElementById('username');
-   var password = document.getElementById('password');
+   var username = document.getElementById('username');console.log(username);
+   var password = document.getElementById('password');console.log(passowrd);
    request.open('POST','http://sonukrj08.imad.hasura-app.io/login',true);
    request.setRequestHeader('content-type','application/json');
    request.send(JSON.stringify({username:username,password:password}));

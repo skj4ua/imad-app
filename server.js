4e9e34d5917e2fb438a4e6c6060bbcb1ec7a9620
sonu jha <sonukrj08@gmail.com>
2018-03-22 16:12:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,11 +59,21 @@ function createTemplate(data){
 
 function hash(input,salt){
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    return ["pkdf2","10000",salt,hashed.toString('hex')].join('$');
     
 }
 app.post('/create-user',function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;
    
+   var salt=  crypto.randomBytes(128).toString('hex');
+   var dbstring = hash(password,salt);
+   pool.query("Insert into 'user' (username,password) vaues($1,$2)",[username],[password],function(err,result){
+       if(err)
+       res.status(500).send(err.toString());
+       else
+       res.send("user successfully created: "+username);
+   });
     
 });
 
@@ -72,19 +82,7 @@ var username = req.body.username;
 var password = req.body.password;
 
 pool.query('Select * from "user" username = $1',[username],function(req,res){
-    
-});
-});
-
-app.get('/hash/:input',function(req,res){
-    var hashedstring = hash(req.params.input,"This is some random string");
-    res.send(hashedstring);
-});
-
-var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM test',function(err,result){
-         if(err){
+   if(err){
           res.status(500).send(err.toString());
           }  
       else if(result.rows.length===0){
@@ -99,6 +97,24 @@ app.get('/test-db',function(req,res){
           else
               res.send(403).send('username/password is incorrect');
           }
+});
+});
+
+app.get('/hash/:input',function(req,res){
+    var hashedstring = hash(req.params.input,"This is some random string");
+    res.send(hashedstring);
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+            
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
     });
 });
 

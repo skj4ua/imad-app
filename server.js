33bae7e293d6a7b79bb7cd287b42d3b334e717e6
sonu jha <sonukrj08@gmail.com>
2018-03-22 17:05:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });

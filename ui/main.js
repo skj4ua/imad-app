d5da368343b45ae2dceefe475d7597649e6cb796
sonu jha <sonukrj08@gmail.com>
2018-03-22 17:28:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,6 +45,7 @@ reg.onclick=function(){
     
    var username = document.getElementById('username').value;
    var password = document.getElementById('password').value;
+   console.log(username);console.log(password);
    request.open('POST','http://sonukrj08.imad.hasura-app.io/create-user',true);
    request.setRequestHeader('Content-Type','application/json');
    request.send(JSON.stringify({username: username,password: password}));

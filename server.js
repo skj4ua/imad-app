f6e1f4f35c2ddcd3fe226303b43894105912ec33
sonu jha <sonukrj08@gmail.com>
2018-03-22 16:55:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,6 +122,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/articles/:article', function (req, res) {
     pool.query("SELECT * FROM article WHERE title = $1 ",[req.params.article],function(err,result){
       if(err){

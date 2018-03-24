8800b9a3b40328408522940bd3334ef4fe9f04ad
sonu jha <sonukrj08@gmail.com>
2018-03-24 17:18:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,40 +4,32 @@ console.log('Loaded!');
 var submit = document.getElementById('submit_btn');
 
 
-submit.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  submit.value = 'Sucess!';
-              } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
-              } else if (request.status === 500) {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              } else {
-                  alert('Something went wrong on the server');
-                  submit.value = 'Login';
-              }
-          }  
-          // Not done yet
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/login', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        submit.value = 'Logged in';
-        
+submit.onclick=function(){
+    var request = new XMLHttpRequest();
+    
+    request.onreadystateexchange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status == 200)
+            alert("logged in Successfully");
+            
+            else if(request.status == 403)
+            alert("username/password is incorrect");
+            
+            else if(request.status  == 500)
+            alert("something went wrong on the server");
+        }
     };
+    
+   var username = document.getElementById('username').value;
+   var password = document.getElementById('password').value;
+   console.log(username);
+   console.log(password);
+   request.open('POST','/login',true);
+   request.setRequestHeader('Content-Type','application/json');
+   request.send(JSON.stringify({username: username,password: password}));
+   submit.value="logged in";
+};
+
 
 var register = document.getElementById('register_btn');
     register.onclick = function () {

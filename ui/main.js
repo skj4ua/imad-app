0337454bdf96a0b5efdca62239885b63f8e70ce6
sonu jha <sonukrj08@gmail.com>
2018-03-24 16:55:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,25 +30,33 @@ submit.onclick=function(){
 };
 
 
-var reg = document.getElementById('reg_btn');
-reg.onclick=function(){
-    var request = new XMLHttpRequest();
-    
-    request.onreadystateexchange = function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-            if(request.status == 200)
-            alert("registration Successfully");
-            else if(request.status == 403)
-            alert("username/password is incorrect");
-            else
-            alert("something went wrong on the server");
-        }
-    };
-    
-   var username = document.getElementById('username').value;
-   var password = document.getElementById('password').value;
-   console.log(username);console.log(password);
-   request.open('POST', '/create-user', true);
+var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
-};
+        register.value = 'Registering...';
+    
+    };
